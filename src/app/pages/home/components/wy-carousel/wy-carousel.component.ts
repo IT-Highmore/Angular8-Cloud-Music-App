bd4a9535aa37334db722aaf4003454b2527dd053
wangcheng.zhao <wangcheng.zhao@qcast.cn>
2021-05-12 17:42:24 +0800

    轮播组件

---
+++ src/app/pages/home/components/wy-carousel/wy-carousel.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, TemplateRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: "app-wy-carousel",
+  templateUrl: "./wy-carousel.component.html",
+  styleUrls: ["./wy-carousel.component.less"],
+  changeDetection: ChangeDetectionStrategy.OnPush, // 其他组件更新不会影响该组件检测刷新，变更检测
+})
+export class WyCarouselComponent implements OnInit {
+  @Input() activeIndex = 0;
+
+  @Output() changeSlide = new EventEmitter<"pre" | "next">();
+
+  @ViewChild("dot", { static: true })
+  dotRef: TemplateRef<any>;
+  constructor() {}
+
+  ngOnInit() {}
+
+  onChangeSlide(type: "pre" | "next") {
+    this.changeSlide.emit(type);
+  }
+}
