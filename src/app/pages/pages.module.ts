bfea5d426fb7dbff72badd2d876c92d78d2da02d
wangcheng.zhao <wangcheng.zhao@qcast.cn>
2021-05-12 11:05:50 +0800

    模块化设计

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { HomeModule } from './home/home.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [HomeModule],
+  exports: [HomeModule],
+})
+export class PagesModule {}
