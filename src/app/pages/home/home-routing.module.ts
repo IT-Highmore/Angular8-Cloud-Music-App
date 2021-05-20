856de6e6e5802e8a6b9f96ba07eea418d5d0cab1
wangcheng.zhao <wangcheng.zhao@qcast.cn>
2021-05-20 15:32:14 +0800

    update file name

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeResolverService } from './home-resolve-service';
+import { HomeResolverService } from './home-resolve.service';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
